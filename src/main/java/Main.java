fd2e5ed7f7d764cd0bf49a333745828d13a2cac7
AlexeyValerich <4352450@gmail.com>
2022-04-04 20:15:55 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+//        String host = "ya.ru";
+//        InetAddress inetAddress = InetAddress.getByName(host);
+//        System.out.println(host + ", ip address: " +
+//                inetAddress.getHostAddress());
+
+//    ServerSocket serverSocket = new ServerSocket(8082); // порт можете выбрать любой в доступном диапазоне 0-65536. Но чтобы не нарваться на уже занятый - рекомендуем использовать около 8080
+//    Socket clientSocket = serverSocket.accept(); // ждем подключения
+//    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+//    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+}
