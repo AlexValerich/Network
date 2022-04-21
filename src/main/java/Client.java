dec6bcd3a6ee1417783d4c5f4591ac1e4da8a2dd
AlexeyValerich <4352450@gmail.com>
2022-04-21 08:44:00 +0300

    new

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,17 +3,21 @@ import java.net.Socket;
 
 public class Client {
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws IOException {
 
-            Socket clientSocket = new Socket("localhost", 8081);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        Socket clientSocket = new Socket("localhost", 8081);
 
-            System.out.println("client start");
-            String word = reader.readLine();
-            out.write(word + "\n");
-            out.flush();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            try (BufferedWriter out = new BufferedWriter
+                    (new OutputStreamWriter(clientSocket.getOutputStream()))) {
+
+                System.out.println("client start");
+                String word = reader.readLine();
+                out.write(word + "\n");
+                out.flush();
+            } catch (IOException e) {
+                System.err.println(e);
+            }
         } catch (IOException e) {
             System.err.println(e);
         }
